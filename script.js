01c30ec625d9c4cbef94af31fee87dc5a5eb0142
CDProjects <caseydentprojects@gmail.com>
2023-11-29 17:43:59 +0200

    Objects array applied for storing facts

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const boxingFacts = [
-  "Muhammad Ali's birth name was Cassius Clay.",
-  "Mike Tyson became the youngest heavyweight champion at the age of 20.",
-  "The 'Thrilla in Manila' was a historic boxing match between Muhammad Ali and Joe Frazier in 1975.",
-  "Floyd Mayweather has an undefeated record of 50-0",
-];
-
-function getRandomBoxingFact() {
-  const randomIndex = Math.floor(Math.random() * boxingFacts.length);
-  return boxingFacts[randomIndex];
-}
-
-const randomFact = getRandomBoxingFact();
-console.log(randomFact);
+    {
+      fact: "Muhammad Ali's birth name was Cassius Clay.",
+      source: "Biography"
+    },
+    {
+      fact: "Mike Tyson became the youngest heavyweight champion at the age of 20.",
+      source: "Sports History"
+    },
+    {
+      fact: "The 'Thrilla in Manila' was a historic boxing match between Muhammad Ali and Joe Frazier in 1975.",
+      source: "Boxing Matches"
+    },
+    {
+      fact: "Floyd Mayweather has an undefeated record of 50-0",
+      source: "Boxing Records"
+    }
+  ];
+  
+  function getRandomBoxingFact() {
+    const randomIndex = Math.floor(Math.random() * boxingFacts.length);
+    return boxingFacts[randomIndex].fact;
+  }
+  
+  const randomFact = getRandomBoxingFact();
+  console.log(randomFact);
+  
